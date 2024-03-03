55b533c9e4fb9f7efb2ce1c6c2be1bba423c441b
Christian <ferchris82@hotmail.com>
2024-03-03 14:40:15 -0500

    Versión 1.0

--- src/main/java/org/chrisferdev/appejb/remote/Main.java
+++ src/main/java/org/chrisferdev/appejb/remote/Main.java
@@ -20,18 +20,19 @@ public class Main {
         env.put("jboss.naming.client.ejb.context", true);*/
         try {
             InitialContext ctx = new InitialContext();
-            service = (ServiceEjbRemote) ctx.lookup("ejb:/appejb-remote/ServiceEjb!org.chrisferdev.webapp.ejb.service.ServiceEjbRemote?stateful");
-            service2 = (ServiceEjbRemote) ctx.lookup("ejb:/appejb-remote/ServiceEjb!org.chrisferdev.webapp.ejb.service.ServiceEjbRemote?stateful");
+            service = (ServiceEjbRemote) ctx.lookup("ejb:/appejb-remote-jaas/ServiceEjb!org.chrisferdev.webapp.ejb.service.ServiceEjbRemote?stateful");
+            service2 = (ServiceEjbRemote) ctx.lookup("ejb:/appejb-remote-jaas/ServiceEjb!org.chrisferdev.webapp.ejb.service.ServiceEjbRemote?stateful");
 
             String saludo = service.saludar("Christian");
             String saludo2 = service.saludar("Karina");
             System.out.println(saludo);
             System.out.println(saludo2);
+            service.listar().forEach(System.out::println);
 
             Producto p = service.crear(new Producto("sandia"));
             System.out.println("nuevo producto: " + p);
 
-            service.listar().forEach(System.out::println);
+
 
         } catch (NamingException e) {
             e.printStackTrace();
